addc43a89e16510d98f0f12b2d6a8b3c650ec309
Christian Zosel <christian@zosel.ch>
2024-12-11 16:15:56 +0100

    db

---
+++ Makefile
@@ -0,0 +1,12 @@
+SHELL:=/bin/sh
+
+.DEFAULT_GOAL := help
+
+.PHONY: help
+help: ## Show the help messages
+        @grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort -k 1,1 | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
+
+
+.PHONY: dbshell
+dbshell:
+	docker-compose exec db psql -U ttmobile
--- config/dev.exs
+++ config/dev.exs
@@ -2,10 +2,10 @@ import Config
 
 # Configure your database
 config :tt_mobile, TtMobile.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "ttmobile",
+  password: "ttmobile",
   hostname: "localhost",
-  database: "tt_mobile_dev",
+  database: "ttmobile",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
